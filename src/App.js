7a4e30f9560c084e9d0efea8289fc03b14718eca
karyllmartinez <reizsakaryllmartinez1230@gmail.com>
2024-05-28 08:03:55 +0800

    Trigger rebuild

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import traveldata from "./traveldata"
+import './index.css';
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
 
-function App() {
+
+export default function App() {
+  const cards = traveldata.map(item => {
+    return (
+        <Card
+            key={item.id}
+            item={item}
+        />
+    )
+  })        
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        
+        <Navbar />
+        <section className="cards-list">
+            {cards}
+        </section>
     </div>
-  );
-}
+  )
+} 
 
-export default App;
